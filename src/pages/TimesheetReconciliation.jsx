e5b04bd850d9371f48a78797c00c1d5568d31755
m.carter <m.carter@omegalife.uk>
2025-11-20 14:44:18 +0000

    File changes

--- src/pages/TimesheetReconciliation.jsx
+++ src/pages/TimesheetReconciliation.jsx
@@ -45,7 +45,10 @@ export default function TimesheetReconciliation() {
 
   const { data: staff = [] } = useQuery({
     queryKey: ['staff'],
-    queryFn: () => base44.entities.Staff.list(),
+    queryFn: async () => {
+      const data = await base44.entities.Staff.list();
+      return Array.isArray(data) ? data : [];
+    },
   });
 
   const { data: shifts = [] } = useQuery({
